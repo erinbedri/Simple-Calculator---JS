75192368e3d1700bd35fce6c147445792dc190ba
erinbedri <erinbedri@gmail.com>
2022-06-15 12:51:13 +0200

    added event listener to buttons
    distiction between 5 different types of buttons

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
 
+keys.addEventListener('click', (e) => {
+    const key = e.target;
+    const action = key.dataset.action;
+
+    if (!action) {
+        console.log('It is a number');
+    }
+
+    if (action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide') {
+        console.log('It is an operation');
+    }
+
+    if (action === 'decimal') {
+        console.log('It is decimal point');
+    }
+
+    if (action === 'clear') {
+        console.log('It is delete button');
+    }
+
+    if (action === 'calculate') {
+        console.log('It is to calculate');
+    }
+
+})
 
