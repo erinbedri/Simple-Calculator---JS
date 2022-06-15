9da2cda8f40b990678381f52a041179af625937b
erinbedri <erinbedri@gmail.com>
2022-06-15 14:08:35 +0200

    added previous display

--- script.js
+++ script.js
@@ -3,36 +3,72 @@ const keys = calculator.querySelector('.calculator__keys');
 const display = calculator.querySelector('.calculator__display');
 
 keys.addEventListener('click', (e) => {
-    const key = e.target;
-    const keyContent = key.textContent;
-    const action = key.dataset.action;
-    const displayedNum = display.textContent;
-
-    if (!action) {
-        if (displayedNum === '0') {
-            display.textContent = keyContent;
-        } else {
-            display.textContent = displayedNum + keyContent;
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const keyContent = key.textContent;
+        const action = key.dataset.action;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+    
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
         }
-    }
+    
+        if (action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide') {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
 
-    if (action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide') {
-        console.log('It is an operation');
-    }
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+    
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+    
+        if (action === 'clear') {
+            console.log('It is delete button');
+        }
+    
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
 
-    if (action === 'decimal') {
-        console.log('It is decimal point');
-    }
+            display.textContent = calculate(firstValue, operator, secondValue);
+        }
+    
+        Array.from(key.parentNode.children)
+            .forEach(k => { k.classList.remove('is-depressed') });
 
-    if (action === 'clear') {
-        console.log('It is delete button');
-    }
+        function calculate(n1, operator, n2) {
+            let result = '';
+            n1 = parseFloat(n1);
+            n2 = parseFloat(n2);
 
-    if (action === 'calculate') {
-        console.log('It is to calculate');
+            if (operator === 'add') {
+                result = n1 + n2;
+            } else if (operator === 'subtract') {
+                result = n1 - n2;
+            } else if (operator === 'multiply') {
+                result = n1 * n2;
+            } else if (operator === 'divide') {
+                if (n2 === 0) {
+                    result = 'You cannot divide by 0';
+                } else {
+                    result = n1 / n2;
+                }
+            }
+            return result;
+        }
+
     }
 
 })
